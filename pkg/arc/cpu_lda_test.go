8b8943518805877321de6b85a3c7b047dac958ed
th1rt3en <giuseppe.richetti@gmail.com>
2023-12-15 15:37:54 +0100

    Add CheckLoadRegisterZeroPage() to avoid duplicate code

--- pkg/arc/cpu_lda_test.go
+++ pkg/arc/cpu_lda_test.go
@@ -66,47 +66,11 @@ func TestLDAImmCanLoadZeroIntoARegister(t *testing.T){
 // Test if the LDA_ZP instruction loads a value succefully into the A register
 func TestLDAZeroPageCanLoadIntoARegister(t *testing.T){
 
-    want := byte(0x72)
-
     cpu := &CPU{}
     cpu.Memory = Memory{}
     cpu.Reset()
 
-    cpuCopy := *cpu
-
-    // start - inline program
-    cpu.Memory.Data[0xFFFC] = instructions.INS_LDA_ZP
-    cpu.Memory.Data[0xFFFD] = 0x42
-    cpu.Memory.Data[0x0042] = want
-    // end - inline program
-
-    // when
-    expectedCycles := 3
-    cyclesUsed := cpu.Execute(expectedCycles)
-
-    if cyclesUsed != expectedCycles {
-        t.Error("Cycles used: ", expectedCycles, ", instead got: ", cyclesUsed)
-    }
-
-    gotA := cpu.A
-    gotZ := cpu.PS.Z
-    gotN := cpu.PS.N
-
-    // then
-    if cpu.A != want {
-        t.Error("A: Want ", want, " instead got ", gotA)
-    }
-
-    if cpu.PS.Z != 0 {
-        t.Error("Z: Want 0, instead got: ", gotZ)
-    }
-
-    if cpu.PS.N != 0 {
-        t.Error("N: Want 0, instead got: ", gotN)
-    }
-
-    // Confront uneffected flags
-    CheckUnmodifiedLDAFlags(cpuCopy, cpu, t)
+    CheckLoadRegisterZeroPage(cpu, instructions.INS_LDA_ZP, &cpu.A, t)
 }
 
 // Test if the LDA_ZPX instruction loads a value succefully into the A register
@@ -382,6 +346,47 @@ func CheckLoadRegisterImmediate(cpu *CPU, opcode int, register *byte, t *testing
     // then
     if *register != 0x72 {
         t.Error("Want: 0x72, got: ", *register)
+    }
+
+    if cpu.PS.Z != 0 {
+        t.Error("Z: Want 0, instead got: ", gotZ)
+    }
+
+    if cpu.PS.N != 0 {
+        t.Error("N: Want 0, instead got: ", gotN)
+    }
+
+    // Confront uneffected flags
+    CheckUnmodifiedLDAFlags(cpuCopy, cpu, t)
+
+}
+
+// This is used to avoid duplicate code
+// TODO: PROBLEM: when using different instructions, cycles needed might change. 
+// Need a way to handle that and operations
+func CheckLoadRegisterZeroPage(cpu *CPU, opcode int, register *byte, t *testing.T){
+
+    // given
+    cpu.Memory.Data[0xFFFC] = byte(opcode)
+    cpu.Memory.Data[0xFFFD] = 0x72
+    cpu.Memory.Data[0x0072] = 0x44
+
+    // when
+    cpuCopy := *cpu
+    expectedCycles := 3
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+
+    if expectedCycles != cyclesUsed{
+        t.Error("Cycles used: ", expectedCycles, ", instead got: ", cyclesUsed)
+    }
+
+    gotZ := cpu.PS.Z
+    gotN := cpu.PS.N
+
+    // then
+    if *register != 0x44 {
+        t.Error("Want: 0x44, got: ", *register)
     }
 
     if cpu.PS.Z != 0 {
