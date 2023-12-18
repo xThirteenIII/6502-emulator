f157219a64eba897ea49b29da3a21150963db447
th1rt3en <giuseppe.richetti@gmail.com>
2023-12-18 20:53:06 +0100

    Add STA function tests

--- pkg/arc/cpu_store_test.go
+++ pkg/arc/cpu_store_test.go
@@ -38,6 +38,85 @@ func TestSTAAbsoluteStoresValueIntoTargetAddress(t *testing.T){
 
 }
 
+func TestSTAAbsoluteXStoresValueIntoTargetAddress(t *testing.T){
+    
+    cpu := &CPU{}
+    cpu.Memory = Memory{}
+    cpu.Reset()
+    cpu.A = 0x09
+
+    CheckStoreRegisterAbsoluteX(cpu, instructions.INS_STA_ABSX, &cpu.A, t)
+
+}
+
+func TestSTAAbsoluteYStoresValueIntoTargetAddress(t *testing.T){
+    
+    cpu := &CPU{}
+    cpu.Memory = Memory{}
+    cpu.Reset()
+    cpu.A = 0x09
+
+    CheckStoreRegisterAbsoluteY(cpu, instructions.INS_STA_ABSY, &cpu.A, t)
+
+}
+
+func TestSTAIndirectXCanLoadIntoARegister(t *testing.T){
+
+    cpu := &CPU{}
+    cpu.Memory = Memory{}
+    cpu.Reset()
+
+    // given
+    cpu.X = 0x04
+
+    // start - inline program
+    cpu.Memory.Data[0xFFFC] = instructions.INS_STA_INDX
+    cpu.Memory.Data[0xFFFD] = 0x02
+    cpu.Memory.Data[0x0006] = 0x00
+    cpu.Memory.Data[0x0007] = 0x80
+    // end - inline program
+
+    expectedCycles := 6
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+    if cyclesUsed != expectedCycles {
+        t.Error("Cycles used: ", cyclesUsed, ", instead expected: ", expectedCycles )
+    }
+
+    if cpu.Memory.Data[0x8000] != cpu.A {
+        t.Error("Want ", cpu.A, "instead got: ", cpu.Memory.Data[0x8000])
+    }
+}
+
+// Test if the LDA_INDY instruction loads a value succefully into the A register
+func TestSTAIndirectYCanLoadIntoARegister(t *testing.T){
+
+    cpu := &CPU{}
+    cpu.Memory = Memory{}
+    cpu.Reset()
+
+    // given
+    cpu.Y = 0x04
+
+    // start - inline program
+    cpu.Memory.Data[0xFFFC] = instructions.INS_STA_INDY
+    cpu.Memory.Data[0xFFFD] = 0x02
+    cpu.Memory.Data[0x0002] = 0x00
+    cpu.Memory.Data[0x0003] = 0x80
+    // end - inline program
+
+    expectedCycles := 5
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+    if cyclesUsed != expectedCycles {
+        t.Error("Cycles used: ", expectedCycles, ", instead got: ", cyclesUsed)
+    }
+
+    if cpu.Memory.Data[0x8004] != cpu.A {
+        t.Error("Want ", cpu.A, "instead got: ", cpu.Memory.Data[0x8004])
+    }
+}
+
 func CheckStoreRegisterZeroPage(cpu *CPU, opcode int, register *byte, t *testing.T){
 
     cpu.Memory.Data[0xFFFC] = byte(opcode)
@@ -88,6 +167,46 @@ func CheckStoreRegisterAbsolute(cpu *CPU, opcode int, register *byte, t *testing
 
     if cpu.Memory.Data[0x8044] != *register {
         t.Error("Expected ", *register, "but got: ", cpu.Memory.Data[0x8044])
+    }
+    
+}
+
+func CheckStoreRegisterAbsoluteX(cpu *CPU, opcode int, register *byte, t *testing.T){
+
+    cpu.X = 0x02
+    cpu.Memory.Data[0xFFFC] = byte(opcode)
+    cpu.Memory.Data[0xFFFD] = 0x44
+    cpu.Memory.Data[0xFFFE] = 0x80
+
+    expectedCycles := 5
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+    if expectedCycles != cyclesUsed{
+        t.Error("Cycles used: ", cyclesUsed, ", instead expected: ", expectedCycles)
+    }
+
+    if cpu.Memory.Data[0x8046] != *register {
+        t.Error("Expected ", *register, "but got: ", cpu.Memory.Data[0x8046])
+    }
+    
+}
+
+func CheckStoreRegisterAbsoluteY(cpu *CPU, opcode int, register *byte, t *testing.T){
+
+    cpu.Y = 0x02
+    cpu.Memory.Data[0xFFFC] = byte(opcode)
+    cpu.Memory.Data[0xFFFD] = 0x44
+    cpu.Memory.Data[0xFFFE] = 0x80
+
+    expectedCycles := 5
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+    if expectedCycles != cyclesUsed{
+        t.Error("Cycles used: ", cyclesUsed, ", instead expected: ", expectedCycles)
+    }
+
+    if cpu.Memory.Data[0x8046] != *register {
+        t.Error("Expected ", *register, "but got: ", cpu.Memory.Data[0x8046])
     }
     
 }
