b76e0f491ba62283cc73c673f330ea3e2d9bb853
xThirteenIII <giuseppe.richetti@gmail.com>
2024-01-27 11:28:26 +0100

    Add ADC_ABS tests

--- pkg/arc/cpu_adc_test.go
+++ pkg/arc/cpu_adc_test.go
@@ -255,6 +255,89 @@ func TestADCZPXAddsCorrectlyWithNoCarryAndOverflow(t *testing.T){
     }
 }
 
+func TestADCABSAddsCorrectlyZeroToZero(t *testing.T){
+
+    cpu := Init6502()
+
+    CheckADCABSExecute(cpu, 0x00, 0x00, 0x00, 4, t)
+
+    CheckIfFollowingFlagsAreSet(t, &cpu.PS.Z)
+    CheckIfFollowingFlagsAreCleared(t, &cpu.PS.V, &cpu.PS.C, &cpu.PS.N)
+}
+
+func TestADCABSAddsCorrectlyWithNoCarryNorOverflow(t *testing.T){
+
+    cpu := Init6502()
+
+    CheckADCABSExecute(cpu, 0x05, 0xF0, 0xF5, 4, t)
+
+    if cpu.PS.C != 0 {
+        t.Error("Carry bit should be 0 but got ", cpu.PS.C)
+    }
+
+    if cpu.PS.V != 0 {
+        t.Error("Overflow bit should be 0 but got ", cpu.PS.V)
+    }
+
+    if cpu.PS.Z != 0 {
+        t.Error("Zero flag should be 0 but got ", cpu.PS.Z)
+    }
+
+    if cpu.PS.N != 1 {
+        t.Error("Negative flag should be 1 but got ", cpu.PS.N)
+    }
+}
+
+func TestADCABSAddsCorrectlyWithCarryAndNoOverflow(t *testing.T){
+
+    cpu := Init6502()
+    CheckADCABSExecute(cpu, 0x05, 0xFB, 0x00, 4, t)
+
+    // Then
+    if cpu.A != 0x00 {
+        t.Error("Accumulator should be 0x00 but got: ", cpu.A)
+    }
+
+    if cpu.PS.C != 1 {
+        t.Error("Carry bit should be 1 but got ", cpu.PS.C)
+    }
+
+    if cpu.PS.V != 0 {
+        t.Error("Overflow bit should be 0 but got ", cpu.PS.V)
+    }
+
+    if cpu.PS.Z != 1 {
+        t.Error("Zero flag should be 1 but got ", cpu.PS.Z)
+    }
+
+    if cpu.PS.N != 0 {
+        t.Error("Negative flag should be 0 but got ", cpu.PS.N)
+    }
+}
+
+func TestADCABSAddsCorrectlyWithNoCarryAndOverflow(t *testing.T){
+
+    // Given
+    cpu := Init6502()
+    CheckADCABSExecute(cpu, 0x7F, 0x01, 0x80, 4, t)
+
+    if cpu.PS.C != 0 {
+        t.Error("Carry bit should be 0 but got ", cpu.PS.C)
+    }
+
+    if cpu.PS.V != 1 {
+        t.Error("Overflow bit should be 1 but got ", cpu.PS.V)
+    }
+
+    if cpu.PS.Z != 0 {
+        t.Error("Zero flag should be 0 but got ", cpu.PS.Z)
+    }
+
+    if cpu.PS.N != 1 {
+        t.Error("Negative flag should be 1 but got ", cpu.PS.N)
+    }
+}
+
 // expectedResult: the value the accumulator should have after add operations,
 // memValue: the value in the memory cell that is added to the accumulator,
 // accumulator: initial value of the register,
@@ -320,6 +403,89 @@ func CheckADCZPXExecute(cpu *CPU, accumulator, memValue, expectedResult byte, ex
     cpu.Memory.Data[0xFFFC] = instructions.INS_ADC_ZPX
     cpu.Memory.Data[0xFFFD] = 0x4F
     cpu.Memory.Data[0x0053] = memValue
+
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+    if expectedCycles != cyclesUsed{
+        t.Error("Cycles used: ", cyclesUsed, ", instead expected: ", expectedCycles)
+    }
+
+    // Then
+    if cpu.A != expectedResult {
+        t.Error("Accumulator should be ", expectedResult, " but got: ", cpu.A)
+    }
+}
+
+// expectedResult: the value the accumulator should have after add operations,
+// memValue: the value in the memory cell that is added to the accumulator,
+// accumulator: initial value of the register,
+// expectedCycles: the number of cycles expected from the instruction execution,
+func CheckADCABSExecute(cpu *CPU, accumulator, memValue, expectedResult byte, expectedCycles int, t *testing.T){
+
+    // Given
+    cpu.A = accumulator
+
+    // When
+    cpu.Memory.Data[0xFFFC] = instructions.INS_ADC_ABS
+    cpu.Memory.Data[0xFFFD] = 0x4F
+    cpu.Memory.Data[0xFFFE] = 0x50
+    cpu.Memory.Data[0x504F] = memValue
+
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+    if expectedCycles != cyclesUsed{
+        t.Error("Cycles used: ", cyclesUsed, ", instead expected: ", expectedCycles)
+    }
+
+    // Then
+    if cpu.A != expectedResult {
+        t.Error("Accumulator should be ", expectedResult, " but got: ", cpu.A)
+    }
+}
+
+// expectedResult: the value the accumulator should have after add operations,
+// memValue: the value in the memory cell that is added to the accumulator,
+// accumulator: initial value of the register,
+// expectedCycles: the number of cycles expected from the instruction execution,
+func CheckADCABSXExecute(cpu *CPU, accumulator, memValue, expectedResult byte, expectedCycles int, t *testing.T){
+
+    // Given
+    cpu.A = accumulator
+    cpu.X = 0x04
+
+    // When
+    cpu.Memory.Data[0xFFFC] = instructions.INS_ADC_ABSX
+    cpu.Memory.Data[0xFFFD] = 0x4F
+    cpu.Memory.Data[0xFFFE] = 0x50
+    cpu.Memory.Data[0x5053] = memValue
+
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+    if expectedCycles != cyclesUsed{
+        t.Error("Cycles used: ", cyclesUsed, ", instead expected: ", expectedCycles)
+    }
+
+    // Then
+    if cpu.A != expectedResult {
+        t.Error("Accumulator should be ", expectedResult, " but got: ", cpu.A)
+    }
+}
+
+// expectedResult: the value the accumulator should have after add operations,
+// memValue: the value in the memory cell that is added to the accumulator,
+// accumulator: initial value of the register,
+// expectedCycles: the number of cycles expected from the instruction execution,
+func CheckADCABSYExecute(cpu *CPU, accumulator, memValue, expectedResult byte, expectedCycles int, t *testing.T){
+
+    // Given
+    cpu.A = accumulator
+    cpu.Y = 0x04
+
+    // When
+    cpu.Memory.Data[0xFFFC] = instructions.INS_ADC_ABSY
+    cpu.Memory.Data[0xFFFD] = 0x4F
+    cpu.Memory.Data[0xFFFE] = 0x50
+    cpu.Memory.Data[0x5053] = memValue
 
     cyclesUsed := cpu.Execute(expectedCycles)
 
